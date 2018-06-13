223906a361b765be1750aee74309acd96d454a39
Matthew Thomas Nelson <achorrath@gmail.com>
2018-06-13 09:57:40 -0700

    Add farm value calculation

--- eggmath.py
+++ eggmath.py
@@ -11,19 +11,27 @@ target_eggs = '5.0q'
 # units: chickens
 target_chickens = '1.0B'
 
+# units: dollars
+target_farm_value = '6.7Sd'
 
 ### Stats
 # units: eggs
 eggs = '3.3q'
 
 # units: chickens
-farm_population = 690676309
+hab_max_capacity = 1461600000
+
+# units: dollars
+farm_value = '75.978sd'
+
+# units: chickens
+farm_population = 82435477
 
 # units: eggs / minute
-egg_laying_rate = '694.026B'
+egg_laying_rate = '140.820B'
 
 # units: chickens / minute / hab
-int_hatchery_rate = 5940
+int_hatchery_rate = 6440
 
 # habs
 habs = 4
@@ -68,15 +76,34 @@ def parse_value(valstr):
     suffix = valstr[-1]
     if suffix in ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9'):
         return int(valstr)
-    mantissa = float(valstr[:-1])
-    if suffix == 'B':
+    if valstr.endswith('B'):
         exponent = 9
-    elif suffix == 'T':
+        suffix_len = 1
+    elif valstr.endswith('T'):
         exponent = 12
-    elif suffix == 'q':
+        suffix_len = 1
+    elif valstr.endswith('q'):
         exponent = 15
+        suffix_len = 1
+    elif valstr.endswith('N'):
+        exponent = 30
+        suffix_len = 1
+    # 'd' is at the end
+    elif valstr.endswith('qd'):
+        exponent = 45
+        suffix_len = 2
+    elif valstr.endswith('sd'):
+        exponent = 51
+        suffix_len = 2
+    elif valstr.endswith('Sd'):
+        exponent = 54
+        suffix_len = 2
+    elif valstr.endswith('d'):
+        exponent = 33
+        suffix_len = 1
     else:
-        raise ValueError('unknown suffix %s (from %s)' % (suffix, valstr))
+        raise ValueError('unknown value %s' % (valstr))
+    mantissa = float(valstr[:-suffix_len])
     return mantissa * 10 ** exponent
 
 
@@ -94,6 +121,12 @@ def format_value(value):
         suffix = 'q'
     if oom3 == 6:
         suffix = 'Q'
+    if oom3 == 8:
+        suffix = 'S'
+    if oom3 == 11:
+        suffix = 'd'
+    if oom3 == 17:
+        suffix = 'sd'
     if suffix is None:
         raise ValueError('result too large: 1000**%d' % oom3)
     return'%.3f%s' % (mantissa, suffix)
@@ -188,6 +221,56 @@ print('target reached in', format_time(t))
 e_final = e_current + e_velocity * t + 0.5 * e_acceleration * t * t
 print('eggs:', format_value(e_final))
 print('farm population:', format_value(c_final))
+
+# units: chickens / minute
+e_velocity_final = e_velocity + e_acceleration * t
+print('egg laying rate', format_value(e_velocity_final))
+print()
+
+
+### Values at farm value target
+# https://www.reddit.com/r/EggsInc/comments/63pfye/patashus_egg_inc_guide_xpost_from_regginc/
+# Farm value is the sum of the three following terms (Thanks /u/dr_hal for finding the third term):
+#
+# income/sec * 54000
+# income/sec/chicken * chicken hab max size * 6000
+# income/sec/chicken * internal hatchery rate/hab/min * 7200000
+# Then:
+# 
+# Multiply by accounting tricks (1.0x if not upgraded, 2.0x if fully upgraded)
+# Multiply by (1.0+0.5 for each egg upgrade above edible egg). Examples: Edible egg = 1.0x, superfood egg = 1.5x, medical egg = 2x, rocket fuel egg = 2.5x ... terraform egg = 7.0x)
+
+# My research says:
+# farm_value = (hab capacity + 9*farm population + 12000*int hatchery rate) * value_scalar
+# farm population is the only part that changes over time
+print('Stats at farm value target')
+
+# units: dollars
+v_current = parse_value(farm_value)
+
+# units: dimensionless
+value_scalar = v_current / (hab_max_capacity + 9 * farm_population + 12000 * int_hatchery_rate)
+
+# units: dollars
+v_final = parse_value(target_farm_value)
+
+# units: chickens
+c_final = (v_final / value_scalar - (hab_max_capacity + 12000 * int_hatchery_rate))/9
+
+# units: minutes
+t = (c_final - c_current) / c_velocity
+print('target reached in', format_time(t))
+
+# units: eggs
+e_final = e_current + e_velocity * t + 0.5 * e_acceleration * t * t
+print('eggs:', format_value(e_final))
+print('farm population:', format_value(c_final))
+if c_final > hab_max_capacity:
+    print('warning: population exceeds hab capacity')
+    
+    # units: dollars
+    v_best = (10 * hab_max_capacity + 12000 * int_hatchery_rate) * value_scalar
+    print('         best possible farm value:', format_value(v_best))
 
 # units: chickens / minute
 e_velocity_final = e_velocity + e_acceleration * t
